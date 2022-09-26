4953fd0aa5284bd8fdd2e000d5a725ef4026554f
mao <2041353520@qq.com>
2022-09-26 11:09:16 +0800

    feat:修改侧边栏样式

---
+++ src/permission.js
@@ -0,0 +1,18 @@
+import router from '@/router'
+import store from '@/store'
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
