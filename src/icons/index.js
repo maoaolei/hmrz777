7be6ad72e51915ded37f028153cec55e599e1559
mao <2041353520@qq.com>
2022-09-26 09:23:07 +0800

    feat:登录完成

--- src/icons/index.js
+++ src/icons/index.js
@@ -5,7 +5,7 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
-console.log(req.keys())
+// console.log(req.keys())
 // console.log(req('./dashboard.svg'))
 // const requireAll = requireContext => requireContext.keys().map(requireContext)
 const requireAll = requireContext => requireContext.keys().map((val) => {
@@ -14,4 +14,4 @@ const requireAll = requireContext => requireContext.keys().map((val) => {
 // map返回一个新数组 长度和原来一样 映射
 // ['./dashboard.svg'].map(requireContext)
 requireAll(req)
-console.log(requireAll(req))
+// console.log(requireAll(req))
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,22 @@
 // 对axios二次封装
 import axios from 'axios'
+import { Message } from 'element-ui'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
+})
+service.interceptors.response.use(response => {
+  // 1.考虑把那些数据抛出去
+  // 2.接口成功 并且业务成功
+  // 3.  没有成功Promise.reject 抛出错误
+  const { message, data, success } = response.data
+  if (success) {
+    return data
+  }
+  Message.error(message)
+  return Promise.reject(new Error(message))
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(error)
 })
 export default service
